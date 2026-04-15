579685ef1546f16b5b1f8ed97101ba8d996477fc
UmutKzl <umut.exe@icloud.com>
2026-04-15 11:14:04 +0300

    Modularize and add package install to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,26 @@
 GITREPO="https://github.com/UmutKzl/linux_dotfiles"
 DOTDIR="$HOME/.dots"
 
-if [ ! -d "$DOTDIR" ]; then
-  git clone $GITREPO $DOTDIR
-fi
+clonerepo() {
+  if [ ! -d "$DOTDIR" ]; then
+    git clone $GITREPO $DOTDIR
+  fi
+}
 
-ln -si $DOTDIR/files/.config/* ~/.config
+linkdots() {
+  ln -si $DOTDIR/files/.config/* ~/.config
+}
+
+installpackages() {
+  if ! command -v dnf >/dev/null; then
+    echo "This script will install dependencies only in Fedora. You should install them manually."
+  fi
+
+  sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+
+  sudo dnf install -y swayosd brightnessctl fastfetch ghostty neovim sway swaybg zip unzip tar
+}
+
+clonerepo
+linkdots
+installpackages
