dc3887bd812e81ca59e3662539ee81296f1fa95f
UmutKzl <umut.exe@icloud.com>
2026-04-16 15:52:59 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,10 +32,55 @@ installpackages() {
 
   else
     echo "No supported package manager found"
+    if command -v pacman >/dev/null; then
+      echo "Install yay and rerun this script."
+    fi
     exit 1
   fi
+}
+
+theme() {
+  if [ ! -d "$HOME/.local/share/themes" ]; then
+    mkdir $HOME/.local/share/themes
+  fi
+
+  cd ~/.local/share/themes
+
+  # Set the root URL
+  export ROOT_URL="https://github.com/catppuccin/gtk/releases/download"
+
+  # Change to the tag you want to download
+  export RELEASE="v1.0.3"
+
+  # Change to suite your flavor / accent combination
+  export FLAVOR="frappe"
+  export ACCENT="blue"
+  curl -LsS -o "catppuccin-${FLAVOR}-${ACCENT}-standard+default.zip" \
+     "${ROOT_URL}/${RELEASE}/catppuccin-${FLAVOR}-${ACCENT}-standard+default.zip"
+
+  # Extract the catppuccin zip file
+  unzip catppuccin-${FLAVOR}-${ACCENT}-standard+default.zip
+
+  # Set the catppuccin theme directory
+  export THEME_DIR="$HOME/.local/share/themes/catppuccin-${FLAVOR}-${ACCENT}-standard+default"
+
+  # Optionally, add support for libadwaita
+  mkdir -p "${HOME}/.config/gtk-4.0" && 
+    ln -sf "${THEME_DIR}/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/assets" &&
+    ln -sf "${THEME_DIR}/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/gtk.css" &&
+    ln -sf "${THEME_DIR}/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/gtk-dark.css"
+  cd -
+}
+
+lastnotes() {
+  echo "TODO: Change theme to Catppuccin by using GTK Settings app."
+  echo "TODO: Change your desktop environment/window manager to Sway."
+
+  echo "Good luck!"
 }
 
 clonerepo
 linkdots
 installpackages
+theme
+lastnotes
