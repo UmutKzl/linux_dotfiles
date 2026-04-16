e21bcd790b957d74fb9f3018e6ef0d42a714333a
UmutKzl <umut.exe@icloud.com>
2026-04-16 23:37:11 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,77 +5,76 @@ GITREPO="https://github.com/UmutKzl/linux_dotfiles"
 DOTDIR="$HOME/.dots"
 
 clonerepo() {
+  echo "Cloning repo..."
   [ -d "$DOTDIR" ] || git clone "$GITREPO" "$DOTDIR"
 }
 
 linkdots() {
+  echo "Linking dotfiles..."
   mkdir -p ~/.config
-  ln -sin "$DOTDIR/files/.config/"* ~/.config/
+  for dir in "$DOTDIR/files/.config/"*; do
+    target="$HOME/.config/$(basename "$dir")"
+    ln -sfn "$dir" "$target"
+  done
 }
 
 installpackages() {
+  echo "Installing packages..."
   if command -v dnf >/dev/null; then
     sudo dnf install -y --nogpgcheck \
       --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-
     sudo dnf copr enable -y solopasha/hyprland
     sudo dnf copr enable -y mradityaalok/satori
-
     sudo dnf install -y \
       swayosd brightnessctl fastfetch ghostty neovim sway swaybg \
-      zip unzip tar luarocks lua nwg-look
-
+      zip unzip tar luarocks lua nwg-look papirus-icon-theme
   elif command -v yay >/dev/null; then
     yay -S --needed \
       swayosd brightnessctl fastfetch ghostty-git neovim sway swaybg \
-      zip unzip tar luarocks lua nwg-look swayidle
-
+      zip unzip tar luarocks lua nwg-look swayidle papirus-icon-theme
   else
-    echo "No supported package manager found"
-    if command -v pacman >/dev/null; then
-      echo "Install yay and rerun this script."
-    fi
+    echo "No supported package manager found. Install yay and rerun."
     exit 1
   fi
 }
 
 theme() {
-  if [ ! -d "$HOME/.local/share/themes" ]; then
-    mkdir $HOME/.local/share/themes
-  fi
+  echo "Setting up themes..."
+  current_dir=$PWD
+  mkdir -p "$HOME/.local/share/themes"
+  cd "$HOME/.local/share/themes"
 
-  cd ~/.local/share/themes
-
-  # Set the root URL
   export ROOT_URL="https://github.com/catppuccin/gtk/releases/download"
-
-  # Change to the tag you want to download
   export RELEASE="v1.0.3"
-
-  # Change to suite your flavor / accent combination
   export FLAVOR="frappe"
   export ACCENT="blue"
-  curl -LsS -o "catppuccin-${FLAVOR}-${ACCENT}-standard+default.zip" \
-     "${ROOT_URL}/${RELEASE}/catppuccin-${FLAVOR}-${ACCENT}-standard+default.zip"
+  
+  zip_file="catppuccin-${FLAVOR}-${ACCENT}-standard+default.zip"
+  curl -LsS -o "$zip_file" "${ROOT_URL}/${RELEASE}/${zip_file}"
+  unzip -o "$zip_file"
+  rm "$zip_file"
 
-  # Extract the catppuccin zip file
-  unzip catppuccin-${FLAVOR}-${ACCENT}-standard+default.zip
-
-  # Set the catppuccin theme directory
   export THEME_DIR="$HOME/.local/share/themes/catppuccin-${FLAVOR}-${ACCENT}-standard+default"
 
-  # Optionally, add support for libadwaita
-  mkdir -p "${HOME}/.config/gtk-4.0" && 
-    ln -sf "${THEME_DIR}/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/assets" &&
-    ln -sf "${THEME_DIR}/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/gtk.css" &&
-    ln -sf "${THEME_DIR}/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/gtk-dark.css"
-  cd -
+  mkdir -p "${HOME}/.config/gtk-4.0"
+  ln -sf "${THEME_DIR}/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/"
+  ln -sf "${THEME_DIR}/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/"
+  ln -sf "${THEME_DIR}/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/"
+
+  cd /tmp
+  git clone https://github.com/catppuccin/papirus-folders.git || true
+  cd papirus-folders
+  sudo cp -r src/* /usr/share/icons/Papirus
+  curl -LO https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-folders/master/papirus-folders && chmod +x ./papirus-folders
+  sudo ./papirus-folders -C cat-mocha-blue --theme Papirus-Dark
+  
+  cd "$current_dir"
 }
 
 lastnotes() {
-  echo "TODO: Change theme to Catppuccin by using GTK Settings app."
-  echo "TODO: Change your desktop environment/window manager to Sway."
-
+  echo "--------------------------------------"
+  echo "TODO: Change theme to Catppuccin using nwg-look or GTK settings."
+  echo "TODO: Log out and select Sway from your display manager."
   echo "Good luck!"
 }
 
