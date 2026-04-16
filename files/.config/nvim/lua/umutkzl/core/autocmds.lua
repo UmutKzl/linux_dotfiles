44789f290230f3405fd446527b6f2af2d6a3ebb9
UmutKzl <umut.exe@icloud.com>
2026-04-16 15:53:29 +0300

    Add Autocmds
    Enable highlight when yanking
    Show diagnostics when hovering

---
+++ files/.config/nvim/lua/umutkzl/core/autocmds.lua
@@ -0,0 +1,21 @@
+vim.api.nvim_create_autocmd("CursorHold", {
+	callback = function()
+		local opts = {
+			focusable = false,
+			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+			border = "rounded",
+			source = "always",
+			prefix = " ",
+			scope = "cursor",
+		}
+		vim.diagnostic.open_float(nil, opts)
+	end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+	end,
+})
