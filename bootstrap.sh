d81af088499a9ccd99c7a7d2f8d15a3ac59ae3f9
UmutKzl <umut.exe@icloud.com>
2026-04-16 11:15:26 +0300

    Update bootstrap.sh to support Arch (my beloved) too

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,39 @@
 #!/usr/bin/env bash
+set -e
 
 GITREPO="https://github.com/UmutKzl/linux_dotfiles"
 DOTDIR="$HOME/.dots"
 
 clonerepo() {
-  if [ ! -d "$DOTDIR" ]; then
-    git clone "$GITREPO" "$DOTDIR"
-  fi
+  [ -d "$DOTDIR" ] || git clone "$GITREPO" "$DOTDIR"
 }
 
 linkdots() {
-  ln -si "$DOTDIR"/files/.config/* ~/.config
+  mkdir -p ~/.config
+  ln -sin "$DOTDIR/files/.config/"* ~/.config/
 }
 
 installpackages() {
-  if ! command -v dnf >/dev/null; then
-    echo "This script will install dependencies only in Fedora. You should install them manually."
+  if command -v dnf >/dev/null; then
+    sudo dnf install -y --nogpgcheck \
+      --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+
+    sudo dnf copr enable -y solopasha/hyprland
+    sudo dnf copr enable -y mradityaalok/satori
+
+    sudo dnf install -y \
+      swayosd brightnessctl fastfetch ghostty neovim swayfx swaybg \
+      zip unzip tar luarocks lua nwg-look
+
+  elif command -v yay >/dev/null; then
+    yay -S --needed \
+      swayosd brightnessctl fastfetch ghostty-git neovim sway swaybg \
+      zip unzip tar luarocks lua nwg-look
+
+  else
+    echo "No supported package manager found"
+    exit 1
   fi
-
-  sudo dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-  sudo dnf copr enable -y solopasha/hyprland
-  sudo dnf copr enable -y mradityaalok/satori
-
-  sudo dnf install -y swayosd brightnessctl fastfetch ghostty neovim swayfx swaybg zip unzip tar luarocks lua nwg-look
 }
 
 clonerepo
